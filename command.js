5920c90ec0f2e252907e4f5e74158e9fd1d1ac1b
Geena Gao <geena.gao@gmail.com>
2018-03-05 11:18:16 -0500

    Create export object

--- command.js
+++ command.js
@@ -1,14 +1,16 @@
-// var exports = module.exports = {};
+module.exports = {
 
-exports.getPwd = function (cmd) {
-  if (cmd === 'pwd') {
-  process.stdout.write(process.cwd());
-  }
-}
+  getPwd: function (cmd) {
+  	if (cmd === 'pwd') {
+  	process.stdout.write(process.cwd());
+  	}
+  },
 
-exports.getDate = function (cmd) {
-  if (cmd === 'date') {
-  var date = new Date();
-  process.stdout.write(String(date));
+  getDate: function (cmd) {
+  	if (cmd === 'date') {
+  	var date = new Date();
+  	process.stdout.write(String(date));
+  	}
   }
+  
 }
